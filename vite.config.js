3234f46ceb3e0b0f7b27d4f05f2ce8c44cdcf810
Neromancy <andrewhermawan1705@gmail.com>
2025-12-28 18:38:21 +0700

    A

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Ganti 'admin-hasil-bumi' dengan nama repository yang kamu buat di GitHub tadi
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/Neromancy/admin-hasil-bumi", 
+  base: "/admin-hasil-bumi/", 
 })
